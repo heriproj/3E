e5a9c4d192e64c4e66b95c47f221392783174f54
Lubos Staracek <lubostar1@gmail.com>
2012-11-20 17:53:38 +0100

    added functions to setup global variables, working memory and run Jess

--- ExpertSystem_SmartPhones/src/JESS/JESS.java
+++ ExpertSystem_SmartPhones/src/JESS/JESS.java
@@ -1,5 +1,6 @@
 package JESS;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.logging.Level;
@@ -8,8 +9,10 @@ import jess.Fact;
 import jess.Filter;
 import jess.Funcall;
 import jess.JessException;
+import jess.RU;
 import jess.Rete;
 import jess.Value;
+import jess.ValueVector;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
@@ -18,10 +21,10 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
  */
 public class JESS {
     public static Rete jess;
-    public static Rete test;
     private static String command=new String();
     private static Iterator results;
-    
+    private static ArrayList<Fact> working_memory=new ArrayList();
+   
     public static void init(){
         JESS.initJess();
     }
@@ -52,13 +55,61 @@ public class JESS {
         System.out.println(test);        
     }
     
-    private static void writeResults(Iterator results){
+    private static void writeResults(Iterator results) throws JessException{
         while(results.hasNext()){ // create Facts from results
-            System.out.println("Result from Java = " + ((HashMap) results.next()).get("name"));
+            HashMap result=(HashMap) results.next();
+            Fact fact=new Fact("device", jess);
+            fact.setSlotValue("name", new Value(result.get("name")));
+            fact.setSlotValue("device_type", new Value(result.get("type")));
+            fact.setSlotValue("manufacturer", new Value(result.get("name")));
+            fact.setSlotValue("OS", new Value(result.get("os")));
+            fact.setSlotValue("display_resolution", new Value(result.get("dislpay")));
+            fact.setSlotValue("storage", new Value(result.get("storage")));
+            fact.setSlotValue("camera", new Value(result.get("camera")));
+
+            ValueVector vv = new ValueVector();
+            Object[] v=(Object[]) result.get("connectivity");
+            System.out.print(" connectivity=");
+            for(int i=v.length-1;i>=0;i--){
+                System.out.print(" " + v[i]);
+                vv.add(new Value(v[i].toString(), RU.STRING));
+            }
+            fact.setSlotValue("connectivity", new Value(vv, RU.LIST));
+
+            v=(Object[]) result.get("io");
+            System.out.print(" io=");
+            for(int i=v.length-1;i>=0;i--){
+                System.out.print(" " + v[i]);
+                vv.add(new Value(v[i].toString(), RU.STRING));
+            }
+            fact.setSlotValue("input_output", new Value(vv, RU.LIST));
+
+            fact.setSlotValue("battery", new Value(result.get("battery")));
+            fact.setSlotValue("price", new Value(result.get("price")));
+
+            working_memory.add(fact);
+
+            System.out.print("name=" + result.get("name"));
+/*            System.out.println("Result from Java = name=" + ((HashMap) results.next()).get("name") +
+                    "type=" + ((HashMap) results.next()).get("type") +
+                    "manufacturer=" + ((HashMap) results.next()).get("manufacturer") +
+                    "OS=" + ((HashMap) results.next()).get("os") +
+                    "display resolution=" + ((HashMap) results.next()).get("display") +
+                    "storage=" + ((HashMap) results.next()).get("storage") +
+                    "camera=" + ((HashMap) results.next()).get("camera") +
+                    "connectivity=" + ((HashMap) results.next()).get("connectivity") +
+                    "io=" + ((HashMap) results.next()).get("io") +
+                    "battery=" + ((HashMap) results.next()).get("battery") +
+                    "price=" + ((HashMap) results.next()).get("price"));*/
         }
     }
     
-    private static void initWorkingMemory(){ // init from results
+    private static void initWorkingMemory() throws JessException{ // init from results
+        for(int i=0;i<=working_memory.size();i++){
+            jess.assertFact(working_memory.get(i));
+        }
+        working_memory=null;
+        working_memory=new ArrayList<>();
     }
     
     public static void setType(String type){
@@ -79,18 +130,175 @@ public class JESS {
               Logger.getLogger(JESS.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    public static void setPrice(int price){
+    
+    public static void setManufacturer(String manufacturer){
+        System.out.println("setting manufacturer...");
         try {
-            jess.reset();
+            jess.reset(); // call reset
+ 
+            command="(defglobal ?*global_manufacturer* = " + manufacturer + ")"; // redefine global
+            jess.executeCommand(command);
             JESS.initWorkingMemory();
             
-            String deffunction="(defglobal ?*global_price* = " + price;            
+            jess.run(); // run JESS
+            // load results from JESS
+            results=jess.getObjects(new Filter.ByClass(HashMap.class));
+            writeResults(results);
+         } catch (JessException ex) {
+              Logger.getLogger(JESS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void setOS(String OS){
+        System.out.println("setting OS...");
+        try {
+            jess.reset(); // call reset
+ 
+            command="(defglobal ?*global_os* = " + OS + ")"; // redefine global
+            jess.executeCommand(command);
+            JESS.initWorkingMemory();
+            
+            jess.run(); // run JESS
+            // load results from JESS
+            results=jess.getObjects(new Filter.ByClass(HashMap.class));
+            writeResults(results);
+         } catch (JessException ex) {
+              Logger.getLogger(JESS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void setDisplay(float min, float max){
+        System.out.println("setting display resolution...");
+        try {
+            jess.reset();
+            
+            String deffunction="(defglobal ?*global_display_min* = " + min + ")";
             jess.executeCommand(deffunction);
+            deffunction="(defglobal ?*global_display_max* = " + max + ")";
+            jess.executeCommand(deffunction);
+            JESS.initWorkingMemory();
             
             jess.run();
+            results=jess.getObjects(new Filter.ByClass(HashMap.class));
          } catch (JessException ex) {
-//            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(JESS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void setStorage(int min, int max){
+        System.out.println("setting storage capacity...");
+        try {
+            jess.reset();
+            
+            String deffunction="(defglobal ?*global_storage_min* = " + min + ")";
+            jess.executeCommand(deffunction);
+            deffunction="(defglobal ?*global_storage_max* = " + max + ")";
+            jess.executeCommand(deffunction);
+            JESS.initWorkingMemory();
+            
+            jess.run();
+            results=jess.getObjects(new Filter.ByClass(HashMap.class));
+         } catch (JessException ex) {
+            Logger.getLogger(JESS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void setCamera(float min, float max){
+        System.out.println("setting camera resolution...");
+        try {
+            jess.reset();
+            
+            String deffunction="(defglobal ?*global_camera_min* = " + min + ")";
+            jess.executeCommand(deffunction);
+            deffunction="(defglobal ?*global_camera_max* = " + max + ")";
+            jess.executeCommand(deffunction);
+            JESS.initWorkingMemory();
+            
+            jess.run();
+            results=jess.getObjects(new Filter.ByClass(HashMap.class));
+         } catch (JessException ex) {
+            Logger.getLogger(JESS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void setConnectivity(ArrayList<String> conns){
+        System.out.println("setting connectivity...");
+        try {
+            jess.reset(); // call reset
+ 
+            command="(defglobal ?*global_connectivity* = (create$";
+            for(int i=0;i<=conns.size();i++){
+                command.concat(" " + conns.get(i));
+            }
+            command.concat("))");
+            jess.executeCommand(command);
+            JESS.initWorkingMemory();
+            
+            jess.run(); // run JESS
+            // load results from JESS
+            results=jess.getObjects(new Filter.ByClass(HashMap.class));
+            writeResults(results);
+         } catch (JessException ex) {
+              Logger.getLogger(JESS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void setIO(ArrayList<String> io){
+        System.out.println("setting input/output...");
+        try {
+            jess.reset(); // call reset
+ 
+            command="(defglobal ?*global_io* = (create$"; // redefine global
+            for(int i=0;i<=io.size();i++){
+                command.concat(" " + io.get(i));
+            }
+            command.concat("))");
+
+            jess.executeCommand(command);
+            JESS.initWorkingMemory();
+            
+            jess.run(); // run JESS
+            // load results from JESS
+            results=jess.getObjects(new Filter.ByClass(HashMap.class));
+            writeResults(results);
+         } catch (JessException ex) {
+              Logger.getLogger(JESS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void setBattery(int min, int max){
+        System.out.println("setting battery...");
+        try {
+            jess.reset();
+            
+            String deffunction="(defglobal ?*global_battery_min* = " + min + ")";
+            jess.executeCommand(deffunction);
+            deffunction="(defglobal ?*global_battery_max* = " + max + ")";
+            jess.executeCommand(deffunction);
+            JESS.initWorkingMemory();
+            
+            jess.run();
+            results=jess.getObjects(new Filter.ByClass(HashMap.class));
+         } catch (JessException ex) {
+            Logger.getLogger(JESS.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void setPrice(int min, int max){
+        System.out.println("setting price...");
+        try {
+            jess.reset();
+            
+            String deffunction="(defglobal ?*global_price_min* = " + min + ")";
+            jess.executeCommand(deffunction);
+            deffunction="(defglobal ?*global_price_max* = " + max + ")";
+            jess.executeCommand(deffunction);
+            JESS.initWorkingMemory();
+            
+            jess.run();
+            results=jess.getObjects(new Filter.ByClass(HashMap.class));
+         } catch (JessException ex) {
+            Logger.getLogger(JESS.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
